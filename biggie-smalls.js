e05359fa1316693e2b2cef03a59b7580b3d0edc2
jreston <jedi.reston@gritlab.ax>
2025-02-10 17:43:37 +0200

    infinity

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -1,6 +1,6 @@
-const smalls = Number.MIN_VALUE / 1;
+const smalls = -Infinity;
 
-const biggie = Number.MAX_VALUE * 1;
+const biggie = Infinity;
 
 /* console.log(smalls, biggie);
 console.log(biggie > 1.7976931348623157e308); // Should log true but logs false */
