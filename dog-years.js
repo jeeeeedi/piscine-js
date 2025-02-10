e35359d0de2cc73a8fb267ff9a33a2fb68224921
jreston <jedi.reston@gritlab.ax>
2025-02-10 16:50:36 +0200

    single quotes!

--- dog-years.js
+++ dog-years.js
@@ -1,12 +1,12 @@
 const arfs = new Map([
-    ["earth", 1],
-    ["mercury", 0.2408467],
-    ["venus", 0.61519726],
-    ["mars", 1.8808158],
-    ["jupiter", 11.862615],
-    ["saturn", 29.447498],
-    ["uranus", 84.016846],
-    ["neptune", 164.79132],
+    ['earth', 1],
+    ['mercury', 0.2408467],
+    ['venus', 0.61519726],
+    ['mars', 1.8808158],
+    ['jupiter', 11.862615],
+    ['saturn', 29.447498],
+    ['uranus', 84.016846],
+    ['neptune', 164.79132],
 ]);
 
 
@@ -17,5 +17,5 @@ function dogYears(planet, ageSec) {
     roundedArfs = Math.round(arfYears * 100) / 100;
     return roundedArfs;
 }
-/* 
-console.log(dogYears("earth", 1000000000)) */
+
+console.log(dogYears('earth', 1000000000))
