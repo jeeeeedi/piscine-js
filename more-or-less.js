de7dd05490065e7f1d47b067ee4aef5f468e9561
jreston <jedi.reston@gritlab.ax>
2025-02-10 15:28:33 +0200

    more-or-less

---
+++ more-or-less.js
@@ -0,0 +1,15 @@
+function more(m) {
+return m + 1;
+}
+
+function less(l) {
+    return l - 1;
+}
+
+function add(add1, add2) {
+    return add1 + add2;
+}
+
+function sub(sub1, sub2) {
+    return sub1 - sub2;
+}
