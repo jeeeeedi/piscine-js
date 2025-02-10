abe4dd36eb88c95311c38079c0306ec9487462a3
jreston <jedi.reston@gritlab.ax>
2025-02-10 14:27:04 +0200

    declaration

---
+++ declarations.js
@@ -0,0 +1,24 @@
+const escapeStr = "`\\/\"'";
+//console.log("`\\/\"'");
+
+const arr = ["4", "'2'"];
+
+const obj = {
+    str: "Hello World!",
+    num: 100,
+    bool: false,
+    undef: undefined,
+};
+
+const nested = {
+    arr: ["4", "undefined", "'2'"],
+    obj: {
+        str: "Hello World!",
+        num: 100,
+        bool: false,
+    }
+}
+
+Object.freeze(nested)
+Object.freeze(arr)
+Object.freeze(obj)
