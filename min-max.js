5d679601fab6c006b75309057397e3843c87bc39
jreston <jedi.reston@gritlab.ax>
2025-02-10 18:24:55 +0200

    minmax

---
+++ min-max.js
@@ -0,0 +1,26 @@
+function max(n1, n2) {
+if (n1 > n2) {
+    return n1;
+} else {
+    return n2;
+}
+}
+
+function min(n1, n2) {
+    if (n1 < n2) {
+        return n1;
+    } else {
+        return n2;
+    }
+}
+
+
+/* console.log(max(0, -2)) // 0
+console.log(max(-1, 10)) // 10
+console.log(max(-13.2, -222)) // -13.2
+console.log(max(132, 133)) // 133
+
+console.log(min(0, -2)) // -2
+console.log(min(-1, 10)) // -1
+console.log(min(-13.2, -222)) // -222
+console.log(min(132, 133)) // 132 */
