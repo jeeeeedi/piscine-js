1f4cd786bc38088652b00397f93718aaa552f324
jreston <jedi.reston@gritlab.ax>
2025-02-12 20:03:29 +0200

    vowel-dots

---
+++ vowel-dots.js
@@ -0,0 +1,8 @@
+function vowelDots(s) {
+    let result = '';
+    let vowels = [aeiou]; // Brackets [abc] specifies matches for any of the characters a, b, or c
+
+    for (let i = 0; i < s.length; i++) {
+        
+    }
+}
