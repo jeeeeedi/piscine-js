acb92c636bec6fbde4e4b852f5116ea25a403ddc
jreston <jedi.reston@gritlab.ax>
2025-02-12 15:34:17 +0200

    let

--- reverser.js
+++ reverser.js
@@ -1,12 +1,12 @@
 function reverse(x) {
     if (typeof x === 'string') {
-        resultStr = '';
+        let resultStr = '';
         for (let i = x.length - 1; i >= 0; i--) {
             resultStr += x[i];
         }
         return resultStr;
     } else if (Array.isArray(x)) {
-        resultArr = [];
+        let resultArr = [];
         for (let i = x.length - 1; i >= 0; i--) {
             resultArr.push(x[i]);
         }
