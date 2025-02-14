89f0d41f2c025b798c2bc0cf1fcb1f8bcacd836f
jreston <jedi.reston@gritlab.ax>
2025-02-14 10:51:30 +0200

    const

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,6 +1,6 @@
 function vowelDots(s) {
     let result = '';
-    let vowels = /[aeiou]/; // Brackets /[abc]/ specifies matches for any of the characters a, b, or c
+    const vowels = /[aeiou]/; // Brackets /[abc]/ specifies matches for any of the characters a, b, or c
 
     for (let i = 0; i < s.length; i++) {
         if (s[i].match(vowels)) {
