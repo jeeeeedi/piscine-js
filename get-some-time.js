287d9a2deeefe83832594b2e4816f6e7b074438c
jreston <jedi.reston@gritlab.ax>
2025-02-17 17:14:33 +0200

    semicolons

--- get-some-time.js
+++ get-some-time.js
@@ -1,19 +1,17 @@
 function firstDayWeek(week, year) {
-    date = new Date(Number(year));
-    result = '';
+    date = new Date(year);
 
     let firstDayofYear = date.getDay(); // Sunday-Saturday: 0-6
     let zeroMonday = (firstDayofYear + 6) % 7; // Convert Sunday=0 to Monday=0
 
-    date.setDate(date.getDate() - zeroMonday - 1)
+    date.setDate(date.getDate() - zeroMonday - 1);
 
     if (week === 1) {
-        date.setDate(date.getDate() + ((week - 1) * 7) + 3)
+        date.setDate(date.getDate() + ((week - 1) * 7) + 3);
     } else {
-        date.setDate(date.getDate() + ((week - 1) * 7) + 1)
+        date.setDate(date.getDate() + ((week - 1) * 7) + 1);
     }
-    result = formatDate(date);
-    return result;
+    return formatDate(date);
 }
 
 // Helper function to format date as 'DD-MM-YYYY'
