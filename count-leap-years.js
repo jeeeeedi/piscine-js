32b7a83a52991651c3007337f37e291b6efa5e96
jreston <jedi.reston@gritlab.ax>
2025-02-17 19:17:36 +0200

    let

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,8 +1,8 @@
 function countLeapYears(d) {
     let year = d.getFullYear();
     let count = 0;
-    for (i = 1; i < year;) {
-        if (isLeapYear(i)) {
+    for (let i = 1; i < year;) {
+        if ((i % 4 === 0 && i % 100 !== 0) || (i % 400 === 0)) { //isLeapYear algo
             count++;
             i += 3;
         } else {
@@ -10,13 +10,4 @@ function countLeapYears(d) {
         }
     }
     return count;
-}
-
-//isLeapYear: returns true if the year of the Date is a leap year.
-function isLeapYear(i) {
-    if ((i % 4 === 0 && i % 100 !== 0) || (i % 400 === 0)) {
-        return true;
-    } else {
-        return false;
-    }
 }
