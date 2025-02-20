b500209eec2ccd0e0297b16fdc3f594269736fbc
jeeeeedi <jedi.reston@gritlab.ax>
2025-02-20 21:50:15 +0200

    fix pimp

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,21 +1,23 @@
 import { styles } from './pimp-my-style.data.js'
 
-function pimp() {
-    let clickCounter = 0;
-    let styleCounter = 0;
-    let btn = document.getElementsByTagName("button")[0];
-    btn.addEventListener("click", () => {
-        if (clickCounter <= styles.length) {
-            btn.classList.add(`${styles[styleCounter]}`);
-            clickCounter++;
-            styleCounter++;
-        } else if (clickCounter >= styles.length && clickCounter <= (styles.length * 2)) {
-            btn.classList.remove(`${styles[styleCounter]}`);
-            clickCounter++;
-            styleCounter--;
-        }
+let clickCounter = 0;
 
-    });
+function pimp() {
+    let btn = document.getElementsByTagName("button")[0];
+    if (!btn.classList.contains("unpimp")) {
+        btn.classList.add(`${styles[clickCounter]}`);
+        clickCounter++;
+    } else {
+        clickCounter--;
+        btn.classList.remove(`${styles[clickCounter]}`);
+        if (clickCounter === 0) {
+            btn.classList.toggle("unpimp");
+        }
+    }
+    if (clickCounter === styles.length) {
+        btn.classList.toggle("unpimp");
+    }
+
 }
 
 export { pimp };
