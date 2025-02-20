84ff80d44a91dcd74c9e1d7e1a38c93c9cba4d35
jeeeeedi <jedi.reston@gritlab.ax>
2025-02-20 02:13:43 +0200

    get-them-all

---
+++ get-them-all.js
@@ -0,0 +1,32 @@
+function getArchitects(arr) {
+    if (Array.isArray(arr)) {
+        let architect = arr.map(architects => architects.tag === 'a');
+        let notArchitect = arr.map(notArchitects => notArchitects.tag !== 'a');
+        return [architect, notArchitect];
+    }
+}
+
+function getClassical(arr) {
+    if (Array.isArray(arr)) {
+        let classical = arr.map(classicals => classicals.classe === 'classical');
+        let notClassical = arr.map(notClassical => notClassical.classe !== 'classical');
+        return [classical, notClassical];
+    }
+}
+
+function getActive(arr) {
+    if (Array.isArray(arr)) {
+        let classical = getClassical(arr)[0];
+        let active = classical.filter(actives => actives.active === true);
+        let nonActive = classical.filter(nonActives => nonActives.active === false);
+        return [active, nonActive];
+    }
+}
+
+function getBonannoPisano(arr) {
+    if (Array.isArray(arr)) {
+        let bon = document.getElementById("BonannoPisano");
+        let notBon = Array.from(document.querySelectorAll('a')).filter(noBon => noBon.id !== 'BonannoPisano' && getActive(arr)[0].some(active => active.id === noBon.id));
+        return [bon, notBon];
+    }
+}
