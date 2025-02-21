90a749c8ac609c18b8a724c5b326e38c5ecc9bc5
jreston <jedi.reston@gritlab.ax>
2025-02-21 14:55:20 +0200

    one style

--- fifty-shades-of-cold.js
+++ fifty-shades-of-cold.js
@@ -3,21 +3,22 @@ import { colors } from './fifty-shades-of-cold.data.js'
 const cold = /(aqua|blue|turquoise|green|cyan|navy|purple)/;
 
 function generateClasses() {
-
+    const style = document.createElement("style");
+    style.textContent = "";
     for (let i = 0; i < colors.length; i++) {
         if (cold.test(colors[i])) {
-            /* const style = document.createElement("style");
-            style.textContent = `.${colors[i]} {
+            style.textContent += `.${colors[i]} {
             background: ${colors[i]};
-          }`;
-            document.head.appendChild(style); */
+          }
+            `;
 
-            const block = document.createElement("div");
-            block.textContent = `${colors[i]}`;
-            block.classList.add(`${colors[i]}`);
-            document.body.appendChild(block);
+            /*      const block = document.createElement("div");
+                 block.textContent = `${colors[i]}`;
+                 block.classList.add(`${colors[i]}`);
+                 document.body.appendChild(block); */
         }
     }
+    document.head.appendChild(style);
 
 }
 
@@ -26,11 +27,11 @@ function generateColdShades() {
 
     for (let i = 0; i < colors.length; i++) {
         if (cold.test(colors[i])) {
-            const style = document.createElement("style");
+           /*  const style = document.createElement("style");
             style.textContent = `.${colors[i]} {
     background: ${colors[i]};
   }`;
-            document.head.appendChild(style);
+            document.head.appendChild(style); */
 
             const block = document.createElement("div");
             block.textContent = `${colors[i]}`;
