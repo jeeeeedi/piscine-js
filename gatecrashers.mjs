f4aa1d70fcdb852c58adf504450694438835c769
jreston <jedi.reston@gritlab.ax>
2025-03-03 19:59:33 +0200

    fix fetch

--- gatecrashers.mjs
+++ gatecrashers.mjs
@@ -2,55 +2,55 @@ import http from "http";
 import { writeFile } from "fs/promises";
 import { Buffer } from "node:buffer";
 
-const USERNAME = ["Caleb_Squires", "Tyrique_Dalton", "Rahima_Young"];
-const PASSWORD = "abracadabra";
-
-const server = http.createServer((req, res) => {
-  const guestName = path.basename(req.url); // Extract guest name from URL
-  const filePath = path.join("./guests", `${guestName}.json`);
-
-  res.setHeader("Content-Type", "application/json"); // Set response header for JSON content
-
-  // Authentication
-  let authHeader = req.headers["authorization"];
-
-  if (!authHeader) {
-    res.statusCode = 401;
-    res.end(JSON.stringify({ error: "unauthorized request" }));
+const bffs = ["Caleb_Squires", "Tyrique_Dalton", "Rahima_Young"];
+const data = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  const fileName = `${req.url.slice(1)}.json`;
+  let authHead = req.headers["authorization"];
+  if (!authHead) {
+    res
+      .writeHead(401, {
+        "Content-Length": Buffer.byteLength(
+          JSON.stringify({ error: "unauthorized" })
+        ),
+      })
+      .end(JSON.stringify({ error: "unauthorized" }));
     return;
   }
-
-  // Extract and decode credentials
-  let credentials = Buffer.from(authHeader.slice(6), "base64")
+  let credentials = Buffer.from(authHead.slice(6), "base64")
     .toString()
     .split(":");
-  if (!USERNAME.includes(credentials[0]) || credentials[1] !== PASSWORD) {
-    res.statusCode = 401;
-    res.end(JSON.stringify({ error: "unauthorized request" }));
+  if (!bffs.includes(credentials[0]) || credentials[1] !== "abracadabra") {
+    res
+      .writeHead(401, {
+        "Content-Length": Buffer.byteLength(
+          JSON.stringify({ error: "unauthorized" })
+        ),
+      })
+      .end(JSON.stringify({ error: "unauthorized" }));
     return;
   }
-
-  let body = req.headers["body"];
-
-  // Write JSON data to guest file (overwrite if it already exists)
-  writeFile(filePath, body)
+  let bodyReq = req.headers["body"];
+  writeFile(`guests/${fileName}`, bodyReq)
     .then(() => {
+      let bodyRes = bodyReq;
       res
-        .writeHead(200, { "Content-Length": Buffer.byteLength(body) })
-        .end(body); // Send the same JSON data back in the response
+        .writeHead(200, {
+          "Content-Length": Buffer.byteLength(bodyRes),
+        })
+        .end(bodyRes);
     })
     .catch((err) => {
-      res.statusCode = 500;
       res
-        .writeHead(500, {
+        .writeHead(401, {
           "Content-Length": Buffer.byteLength(
-            JSON.stringify({ error: "error" })
+            JSON.stringify({ error: "unauthorized" })
           ),
         })
-        .end(JSON.stringify({ error: "error" }));
+        .end(JSON.stringify({ error: "unauthorized" }));
     });
-});
-
+};
+const server = http.createServer(data);
 server.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
