a1b9471e34b93c696a0fb6115386c69eb455caa4
jreston <jedi.reston@gritlab.ax>
2025-03-03 16:57:04 +0200

    fix filepath

--- tell-me-vip.mjs
+++ tell-me-vip.mjs
@@ -1,40 +1,31 @@
-import { readdir } from "node:fs/promises";
-import { readFile } from "node:fs/promises";
-import { writeFile } from "node:fs/promises";
+import { readdir, readFile, writeFile } from "node:fs/promises";
+import { argv } from "node:process";
+const guestsPath = argv[2] ? argv[2] : null;
 let result = "";
 
 try {
-  const files = await readdir("./tell-me-how-many/guests");
-  let firstName = "";
-  let lastName = "";
-  let fullName = "";
+  const files = await readdir(guestsPath);
   let names = [];
 
   for (const fileName of files) {
-    let filePath = "./tell-me-how-many/guests/" + fileName; // go to each file
+    let filePath = `${guestsPath}/${fileName}`; // Dynamic path
     let content = await readFile(filePath, { encoding: "utf8" });
     let jsonData = JSON.parse(content); // Parse content as JSON
 
     // Check if "answer" key has "yes" value
     if (jsonData.answer === "yes") {
-      // get name
-      fullName = fileName.slice(0, -5).split("_");
-      firstName = fullName[0];
-      lastName = fullName[1];
+      let [firstName, lastName] = fileName.slice(0, -5).split("_"); // get name
       names.push(`${lastName} ${firstName}`);
     }
   }
 
-  let sorted = names.sort(); // sort
+  let sorted = names.sort(); // sort alpha
 
   // print line by line
-  let count = 0;
-  let lines = [];
-  for (const name of sorted) {
-    count++;
-    lines.push(`${count}. ${name}`);
-  }
-  result = lines.join("\n");
+  result = sorted
+    .map((name, index) => `${index + 1}. ${name}`)
+    .join("\n")
+    .trim();
 
   await writeFile("vip.txt", result); // write to file
 } catch (err) {
